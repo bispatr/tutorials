1d3cd13880c629e873e09d275f8cc7e23f89f28e
bispatr <bishnusec@gmail.com>
2022-12-12 05:07:08 +0530

    added files for springboot

---
+++ springboottutorials/Ch06_Spring_security_with_JWT/jwtjpa/src/main/java/com/jwtjpa/JwtjpaApplication.java
@@ -0,0 +1,13 @@
+package com.jwtjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtjpaApplication.class, args);
+	}
+
+}
---
+++ springboottutorials/Ch06_Spring_security_with_JWT/jwtjpa/src/test/java/com/jwtjpa/JwtjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
