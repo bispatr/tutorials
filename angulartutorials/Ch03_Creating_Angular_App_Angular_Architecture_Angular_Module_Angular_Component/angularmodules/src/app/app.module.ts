568b5804808dcd8dd85c4a9cc905cefe56c91ba0
bispatr <bishnusec@gmail.com>
2023-01-31 17:04:27 +0530

    added core java projects

--- angulartutorials/Ch03_Creating_Angular_App_Angular_Architecture_Angular_Module_Angular_Component/angularmodules/src/app/app.module.ts
+++ angulartutorials/Ch03_Creating_Angular_App_Angular_Architecture_Angular_Module_Angular_Component/angularmodules/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TestmoduleModule } from 'src/modules/testmodule/testmodule.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    TestmoduleModule,
     AppRoutingModule
   ],
   providers: [],
